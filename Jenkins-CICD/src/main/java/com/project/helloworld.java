de662a2948b8a509184e4e72ff32847b226eb405
Subasangeeth <142552707+subasangeeth@users.noreply.github.com>
2025-09-29 22:11:30 +0530

    Update helloworld.java

--- Jenkins-CICD/src/main/java/com/project/helloworld.java
+++ Jenkins-CICD/src/main/java/com/project/helloworld.java
@@ -1,16 +1,1 @@
-package com.project;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class helloworld {
-	
-	@GetMapping("")
-	public String hello()
-	{
-		return "Hello, World!";
-	}
-
-}
