8b4ee76dfe669eae6aaba96ffe44af36110ee4fa
Subasangeeth <142552707+subasangeeth@users.noreply.github.com>
2025-09-30 05:43:05 +0530

    Update main.tf

--- Jenkins-CICD/terr_ible/main.tf
+++ Jenkins-CICD/terr_ible/main.tf
@@ -49,6 +49,15 @@ resource "aws_security_group" "TF_SG" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ingress {
+    description      = "HTTP"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
 
   ingress {
